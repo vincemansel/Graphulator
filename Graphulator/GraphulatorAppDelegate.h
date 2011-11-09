b06535d0afdc677a92a9139530b0bf7e101acf60
Vince Mansel <vincemansel@gmail.com>
2011-11-08 17:14:07 -0800

    Initial Commit

---
+++ Graphulator/GraphulatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GraphulatorAppDelegate.h
+//  Graphulator
+//
+//  Created by Vince Mansel on 11/8/11.
+//  Copyright (c) 2011 Wave Ocean Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphulatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Graphulator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Graphulator
+//
+//  Created by Vince Mansel on 11/8/11.
+//  Copyright (c) 2011 Wave Ocean Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GraphulatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GraphulatorAppDelegate class]));
+    }
+}
